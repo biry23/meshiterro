c467f3b5e9a01651fd35fc6ee61c81883737f183
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-02-27 09:07:45 +0000

    [Finish]4-8モデル・テーブル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     about_path
   end
-  
-  
+
+
   def after_sign_out_path_for(resouroe)
     about_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
