4cc3e7c2e0b086e74432eced55ee6678aa7491f4
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-02-28 07:35:03 +0000

    [Finish]4-13投稿削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'post_images/index'
-  get 'post_images/show'
-  get 'post_images/new'
   root to: 'homes#top'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
   get 'homes/about' => 'homes#about', as: 'about'
 
