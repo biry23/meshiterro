10cab6e7ef5257a02ec6d17306549130c61b7843
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2022-03-01 06:42:22 +0000

    [Finish]4-18コメント投稿,削除機能追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,8 @@ class PostImage < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+
 
   def get_image
     if image.attached?
@@ -11,13 +13,13 @@ class PostImage < ApplicationRecord
       'no_image.jpg'
     end
   end
-   
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
-  end 
+  end
 
 end
